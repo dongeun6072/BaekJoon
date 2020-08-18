5aae21bc81ba9e8c5b7ee5aa2ee23c3043085d30
dongeun <dongeun6072@naver.com>
2020-08-19 00:49:47 +0900

    4673번

---
+++ 4673_셀프 넘버.py
@@ -0,0 +1,11 @@
+num = set(range(1,100))
+generated_num = set()
+for i in range(1,100):
+    print(i)
+    for j in str(i):
+        print("{0} {1}".format(int(j),i))
+        i += int(j)
+    generated_num.add(i)
+self_num = num - generated_num
+for k in sorted(self_num):
+    print(k)
