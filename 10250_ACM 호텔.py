a7243d33253292ab1c5575370374c7054403f3f1
dongeun <dongeun6072@naver.com>
2020-09-01 12:20:27 +0900

    10250번

---
+++ 10250_ACM 호텔.py
@@ -0,0 +1,17 @@
+inp = int(input())
+
+for j in range(inp):
+    H,W,N = map(int,input().split())
+    f = 0
+    ho = 0
+    if N % H == 0:
+        f = H * 100
+        ho = N // H
+    else:
+        f = (N % H)* 100
+        ho = 1+ N//H
+        
+
+    print(f+ho)       
+       
+   
